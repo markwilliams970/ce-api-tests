f5956a8929607483b028652037d592235727f2e5
Greg Lindahl <greglindahl410@gmail.com>
2014-09-11 15:35:30 -0600

    Working first rspec against sfdcmarketingcloud contact

--- marketing/salesforce/salesforce_spec.rb
+++ marketing/salesforce/salesforce_spec.rb
@@ -1,8 +1,13 @@
 require 'rspec'
 require 'httpclient'
 require 'pp'
+require 'json'
 
 require File.dirname(__FILE__) + "/auth_keys.rb"
+
+def make_req_url(hub, endpoint, version)
+	return "#{@base_url}/elements/api-#{version}/hubs/#{hub}/#{endpoint}"
+end
 
 describe "Marketing Hub Salesforce Contact CRUD Tests" do
 
@@ -13,46 +18,37 @@ describe "Marketing Hub Salesforce Contact CRUD Tests" do
 	@my_http_client.receive_timeout                = 300
 	@my_http_client.send_timeout                   = 300
 	
+	# Elements Server
+	@base_url                                      = "https://api.cloud-elements.com"
+	
 	# Authentication information
 	@element_token                                  = $salesforce_element_token
 	@user_secret                                    = $salesforce_user_secret
 	@authentication_key                             = "Element #{@element_token}, User #{@user_secret}"
 	
-	puts @authentication_key
-	
 	@req_headers = {}
 	@req_headers["Authorization"]                  = @authentication_key
 	@req_headers["Content-Type"]                   = "application/json"
+	
+	# JSON data
+	contact_file                                   = File.dirname(__FILE__) + "/json/contactsfdcmar.json"
+	@contact_json                                  = File.read(contact_file)
   end
 
   it "should create a contact" do
-  	contact = {}
-  	contact["firsName"] = "Your"
-  	contact["lastName"] = "Mom"
-  	contact["phone"] = "777-777-7777"
-  	contact["email"] = "yourmom@acmedataservices"
   	
-  	address = {}
-  	address["stateOrProvince"] = "CO"
-  	address["postalCode"] = "80212"
-  	address["country"] = "US"
-  	
-  	contact["address"] = address
-  	
-  	contact_url = "https://api.cloud-elements.com/elements/api-v2/hubs/marketing/contacts"
+  	contact_url = make_req_url("marketing", "contacts", "v2")
   	
   	req_method = :post
 
 	req_args = {
 		:header => @req_headers,
-		:body   => contact
+		:body   => @contact_json
 	}
 
 	response = @my_http_client.request(req_method, contact_url, req_args)
-	puts response.inspect
-	var1 = 1
-	var2 = 2
-  	expect(var1).to eq(var2)
+	response_http_status = response.status_code
+  	expect(response_http_status).to eq(200)
   end
 
 end

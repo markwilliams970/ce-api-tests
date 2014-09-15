4289dcfd54d5aacafbc767d5ee32e259f476a63c
Mark Williams <markwilliams@markwilliams-mbp.local>
2014-09-15 09:34:43 -0600

    Re-factored make_req to be generic

--- marketing/salesforce/salesforce_spec.rb
+++ marketing/salesforce/salesforce_spec.rb
@@ -6,51 +6,61 @@ require 'json'
 require File.dirname(__FILE__) + "/auth_keys.rb"
 
 def make_req_url(hub, endpoint, version)
-	return "#{@base_url}/elements/api-#{version}/hubs/#{hub}/#{endpoint}"
+   return "#{@base_url}/elements/api-#{version}/hubs/#{hub}/#{endpoint}"
 end
 
-def make_post_req(json_file_name, hub, endpoint, version)
+def make_req(req_type, hub, endpoint, version, json_file_name)
 
-	json_file                                   = File.dirname(__FILE__) + "/json/#{json_file_name}"
-	json_string                                 = File.read(json_file)
+   if req_type == :post then
+      json_file                = File.dirname(__FILE__) + "/json/#{json_file_name}"
+      json_string              = File.read(json_file)
+      args                     = {:header => @req_headers, :body => json_string}
+   elsif req_type == :get then
+      args                     = {:header => @req_headers}
+   end
 
-	post_req = {
-		:method => :post,
-		:url => make_req_url(hub, endpoint, version),
-		:args => {:header => @req_headers, :body => json_string}
-	}
-	return post_req
+   req_url = make_req_url(hub, endpoint, version)
+
+   req = {
+      :method => req_type,
+      :url => req_url,
+      :args => args
+   }
+
+   puts "Testing #{req_type} against #{req_url}..."
+   if req_type == :post then puts "Body: #{json_string}" end
+   return req
 end
 
 describe "Marketing Hub Salesforce Contact CRUD Tests" do
 
   before :all do
-	@ce_http_client                                = HTTPClient.new
-	@ce_http_client.protocol_retry_count           = 2
-	@ce_http_client.connect_timeout                = 300
-	@ce_http_client.receive_timeout                = 300
-	@ce_http_client.send_timeout                   = 300
-	
-	# Elements Server
-	@base_url                                      = "https://api.cloud-elements.com"
-	
-	# Authentication information
-	@element_token                                  = $salesforce_element_token
-	@user_secret                                    = $salesforce_user_secret
-	@authentication_key                             = "Element #{@element_token}, User #{@user_secret}"
-	
-	@req_headers = {}
-	@req_headers["Authorization"]                  = @authentication_key
-	@req_headers["Content-Type"]                   = "application/json"
-	
+   @ce_http_client                                = HTTPClient.new
+   @ce_http_client.protocol_retry_count           = 2
+   @ce_http_client.connect_timeout                = 300
+   @ce_http_client.receive_timeout                = 300
+   @ce_http_client.send_timeout                   = 300
+   
+   # Elements Server
+   @base_url                                      = "https://api.cloud-elements.com"
+   
+   # Authentication information
+   @element_token                                 = $salesforce_element_token
+   @user_secret                                   = $salesforce_user_secret
+   @authentication_key                            = "Element #{@element_token}, User #{@user_secret}"
+   
+   @req_headers = {}
+   @req_headers["Authorization"]                  = @authentication_key
+   @req_headers["Content-Type"]                   = "application/json"
+   
   end
 
   it "should create a contact" do
-  	
-	contact_request      = make_post_req("contactsfdcmar.json", "marketing", "contacts", "v2")
-	response             = @ce_http_client.request(contact_request[:method], contact_request[:url], contact_request[:args])
-	response_http_status = response.status_code
-  	expect(response_http_status).to eq(200)
+   
+   contact_request      = make_req(:post, "marketing", "contacts", "v2", "contactsfdcmar.json")
+   response             = @ce_http_client.request(contact_request[:method], contact_request[:url], contact_request[:args])
+   response_http_status = response.status_code
+   expect(response_http_status).to eq(200)
   end
 
 end
